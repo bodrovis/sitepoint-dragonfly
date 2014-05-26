7f363d4408eddd270357da915ee56bf9e52983b6
bodrovis <ibodrov@campaigner.ru>
2014-05-26 19:33:51 +0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :photos
+  resources :photos, only: [:new, :create, :index]
 
   root to: 'photos#index'
   # The priority is based upon order of creation: first created -> highest priority.
