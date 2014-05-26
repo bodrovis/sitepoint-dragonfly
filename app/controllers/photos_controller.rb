927cd2f2452837344b439fe1f58bd5f27b9fce4f
bodrovis <ibodrov@campaigner.ru>
2014-05-27 00:26:37 +0400

    preview

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,6 +20,6 @@ class PhotosController < ApplicationController
   private
 
   def photo_params
-    params.require(:photo).permit(:image, :title)
+    params.require(:photo).permit(:image, :title, :retained_image, :remove_image)
   end
 end
